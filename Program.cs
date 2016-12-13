8ad2ff485d62a958b10dca666051982fb468f1b8
spirit11 <mps_ymg@rambler.ru>
2016-12-13 11:12:00 +0300

    minimal project

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FloatPointDiff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = double.Parse("-0.41404453737343927");
+            var i = double.Parse("-0.11823238255337409");
+
+            Console.WriteLine("Atan2 {0:R}", Math.Atan2(i, r));
+            Console.WriteLine("Atan {0:R}", Math.Atan(i / r));
+            Console.WriteLine("Atan-Pi {0:R}", Math.Atan(i / r) - Math.PI);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ compile_and_run.bat
@@ -0,0 +1,3 @@
+"c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+rem "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" 
+floatpointdiff.exe
